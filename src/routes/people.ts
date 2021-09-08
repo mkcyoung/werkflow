bb4245224f6a6f5c627ae4a4176947de3ff59385
mkcyoung <m.c.young0@gmail.com>
2021-09-08 16:28:25 -0600

    when people added, tasks get updated

--- src/routes/people.ts
+++ src/routes/people.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express, { Request, Response } from 'express'
+import { ObjectId } from 'mongoose';
 // import taskService from '../services/taskService'
 import Person from '../models/person'
+import Task from '../models/task'
 
 const router = express.Router();
 
@@ -14,6 +17,19 @@ router.get('/', [], async (_req: Request, res: Response ) => {
 
 });
 
-
+router.post('/', [], async (req: Request, res: Response) => {
+    const person = new Person(req.body)
+  
+    const savedPerson = await person.save()
+  
+    savedPerson.tasks.forEach( async ( taskId : ObjectId ) => {
+      const task = await Task.findById(taskId)
+      if (task) {
+        task.people = task.people.concat(savedPerson._id)
+        await task.save()
+      }
+    })
+    return res.status(201).json(savedPerson)
+})
 
 export default router;
